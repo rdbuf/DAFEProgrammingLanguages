b254bed6d68c73d2dc84ff57eb6f13df80e05289
Ilya Pikulin <ilya.pikulin@gmail.com>
2018-12-17 13:00:51 +0300

    Employ cond macro

--- lisp-10.lisp
+++ lisp-10.lisp
@@ -1,9 +1,8 @@
 (defun count-atoms (lst)
-	(if (null lst)
-		0
-		(if (atom lst)
-			1
-			(+ (count-atoms (cdr lst)) (count-atoms (car lst))))))
+	(cond
+		((null lst) 0)
+		((atom lst) 1)
+		(t (+ (count-atoms (car lst)) (count-atoms (cdr lst))))))
 
 (assert (equal (count-atoms '(1 (2 3 4) 5 (5 4 nil))) 7))
 (assert (equal (count-atoms '(1 2 3)) 3))
