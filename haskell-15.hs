158d2a9f4182f37407140f3fea7f3c4d848d79a8
Vlad Mavromatidi <47668489+De4thtrap@users.noreply.github.com>
2019-05-25 19:57:52 +0300

    Delete haskell-15.hs

--- haskell-15.hs
+++
@@ -1,4 +0,0 @@
-delete [] = error "No list"
-delete x = if length x < 3 then error "No elem"
-                           else if length x == 3 then tail (tail x)
-                                               else [head x] ++ delete (tail x)
